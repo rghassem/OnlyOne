577887423428cb1cb51686206cd059514b029c60
Reza Ghassemi <rghassem@gmail.com>
2020-03-11 01:36:02 -0700

    Implement new tutorial of 15 levels

--- src/levels.ts
+++ src/levels.ts
@@ -1,16 +1,10 @@
 import { maxX, maxY, Gameboard } from "./board";
 import { LetterFrequencyMap } from "./letterEntity";
 import { LevelTypes } from "./levelTypes";
+import { handmadeLevel } from "./handmadeLevels";
 
 export const MaxLevels = 999;
 export const LevelSequenceLength = 10;
-
-const levels = new Map<number, string>();
-levels.set(0, levelOne());
-levels.set(1, levelTwo());
-levels.set(2, levelThree());
-levels.set(3, levelFour());
-levels.set(4, levelFive());
 
 export interface LevelSequence {
     frequencies: keyof typeof LevelTypes,
@@ -25,8 +19,10 @@ export function getLevel(level: number, forceFrequencyMap?: LetterFrequencyMap) 
     if (forceFrequencyMap) {
         return Gameboard.fromSeed(level, forceFrequencyMap);
     }
-    else if (levels.has(level)) {
-        return Gameboard.fromString(levels.get(level)!, level, LevelTypes.DefaultLetterFrequency);
+
+    const handmade = handmadeLevel(level);
+    if (handmade) {
+        return Gameboard.fromString(handmade, level, LevelTypes.DefaultLetterFrequency);
     }
     else {
         const sequenceIndex = Math.floor(level / 10);
@@ -66,65 +62,4 @@ export function loseScreen() {
         + 'TRYAGAIN' +
         ' '.repeat(maxX * Math.floor(maxY / 2)),
         0, LevelTypes.DefaultLetterFrequency);
-}
-
-function levelOne() {
-    return ' '
-        .repeat(maxX * (maxY - 2))
-        + '  012   '
-        + 'IIWIIIIL';
-}
-
-function levelTwo() {
-    const result = ' '
-        .repeat(maxX * (maxY - 6))
-        + 'MUSICIBY'
-        + 'PERITUNE'
-        + 'I2IIIII2'
-        + 'IDIIIIID'
-        + 'III01III'
-        + 'RIIIIIII'
-
-    return result;
-}
-
-function levelThree() {
-    const result = ' '
-        .repeat(maxX * (maxY - 6))
-        + 'BY  REZA'
-        + 'AND MARK'
-        + '0I0IIIII'
-        + 'DIW1IIIW'
-        + 'IIID2III'
-        + 'WIUIICIU';
-    return result;
-}
-
-function levelFour() {
-    const result = ' '
-        .repeat(maxX * (maxY - 7))
-        + 'IIIIII1I'
-        + 'IIIIIII1'
-        + 'IIIIIICI'
-        + 'IIIIIIIY'
-        + 'R0IIIRCI'
-        + 'IYID2IIC'
-        + 'RIUIICWW';
-    return result;
-}
-
-function levelFive() {
-    const result = ' '
-        .repeat(maxX * (maxY - 10))
-        + '**0I1I2*'
-        + '1IIII0I1'
-        + 'III2IICI'
-        + '********'
-        + '********'
-        + '********'
-        + '********'
-        + '********'
-        + '********'
-        + '********';
-    return result;
 }
